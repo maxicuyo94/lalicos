cce28e06c85150a415e290564f3f929553aa3493
Maximiliano Mendez <maximiliano.mendez@vemobility.net>
2022-11-13 14:33:28 -0300

    reformat

--- index.js
+++ index.js
@@ -38,3 +38,19 @@ darkMode.addEventListener('click', () => {
 })
 
 
+
+const titulo = document.getElementById('t1');
+
+// alternar elt tiulo entre Equipo Lali Cosmetics y Tu Cosmetica en casa
+setInterval(() => {
+  setTimeout(() => {
+    titulo.innerHTML = 'Equipo Lali Cosmetics';
+  }, 2000);
+  setTimeout(() => {
+    titulo.innerHTML = 'Tu Cosmetica en casa';
+  }, 1000);
+}, 3000);
+
+
+
+
---
+++ page/script.js
@@ -0,0 +1,17 @@
+// alert("Hola, preparate para mejorar la calidad de tu piel");
+// let agua = prompt("Ingrese en ml la cantidad de agua que consume por día");
+// let peso = prompt("Ingrese su peso en kg");
+
+
+// function calcular(agua, peso) {
+//   let resultado = (agua * peso);
+//   if (agua > 2000) {
+//     alert("¡Cuidado - parece que necesitas beber más agua de lo que pensabas para mantener la salud de tu piel!")
+//   } else {
+//     alert("Tomas suficiente agua, lo cual es crucial para una piel bonita");
+//   }
+//   return console.log(resultado);
+// }
+
+// calcular(agua, peso)
+
